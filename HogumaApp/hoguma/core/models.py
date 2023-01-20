d1be91fa6086d1df33fa30fe0a738bfa07479a6a
Jose Angel <doradojoseangel@gmail.com>
2023-01-20 16:07:59 +0100

    Model of users and connection to postgreSQL done

--- HogumaApp/hoguma/core/models.py
+++ HogumaApp/hoguma/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class users(models.Model):
+    nombre=models.CharField(max_length=30)
+    apellidos=models.CharField(max_length=70)
+    email=models.EmailField()
+    password=models.CharField(max_length=30)
