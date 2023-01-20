9a33bb18559f104204876f15fd119b8151de7079
Jose Angel <doradojoseangel@gmail.com>
2023-01-20 14:26:40 +0100

    Integration postgreSQL

--- HogumaApp/hoguma/hoguma/settings.py
+++ HogumaApp/hoguma/hoguma/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'hoguma.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hoguma',
+        'USER': 'jose',
+        'PASSWORD': 'Usuario77',
+        'HOST': 'localhost',
+        'DATABASE_PORT': '5432',
     }
 }
 
